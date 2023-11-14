dbfe7653e63656d31e9c3ae77e677807bc3250b3
Anton Semenov <Anton.Semenov@1e.com>
2023-11-14 19:26:45 +0000

    Domain etc

---
+++ src/MKopa.SMSWorker/MKopa.SMS.Worker.Lib/DomainModel/SendSmsCommand.cs
@@ -0,0 +1,27 @@
+﻿using PhoneNumbers;
+using Visus.Cuid;
+
+namespace MKopa.SMS.Worker.Lib.DomainModel
+{
+    public class SendSmsCommand
+    {
+        Cuid2 CorrelationId { get; }
+        PhoneNumber PhoneNumber { get; }
+        string Text { get; }
+
+        public SendSmsCommand(Cuid2 correlationId, PhoneNumber phoneNumber, string text)
+        { 
+            PhoneNumber = phoneNumber;
+            CorrelationId = correlationId;
+            Text = text;
+        }
+
+        public void Validate()
+        {
+            if (Text == null) throw new ArgumentNullException("SMS text can not be null!");
+            if (PhoneNumber == null) throw new ArgumentNullException("Phone number can not be null!");
+            if (CorrelationId == default) throw new ArgumentException("Correlation id must be set!");
+            if (Text.Length > 160) throw new ArgumentException("SMS text too long!");
+        }
+    }
+}
